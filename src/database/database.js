f9b06429aa44f9959d3f24700483388e7ffd3de4
Jaciel castro <jacielcastro100@gmail.com>
2026-06-10 20:58:24 -0300

    :book: Atualizado

--- src/database/database.js
+++ src/database/database.js
@@ -1,18 +1,36 @@
 import { Sequelize } from 'sequelize'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // Corrige o __dirname no ESModule
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // Configuração do banco SQLite
-const sequelize = new Sequelize({
+/*const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: path.join(__dirname, '..', 'database.sqlite'),
     logging: false
 })
+*/
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL,
 
+    {
+    dialect : 'postgres',
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false
+    }
+
+)
 // Função para sincronizar o banco
 export async function sincronizarBD() {
     try {
