a3b5a29bd75566619263c04f65634896be69039c
jacielcastro <jacielcastro100@gmail.com>
2026-08-02 16:30:04 -0300

    :white_check_mark: ATUALIZAÇÃO DO PROJETO

--- src/public/src/Funcao/funcao_telas.js
+++ src/public/src/Funcao/funcao_telas.js
@@ -1,6 +1,6 @@
 
 export function troca_Tela() {
     alert('Tela de cadastro de administrador');
-    window.location.href = '/administrador/cadastro.html';
+    window.location.href = 'cadastro_usuario.html';
 }
 window.troca_Tela = troca_Tela;
