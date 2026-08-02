a8632d50fbfb1f3d586edfb1957de8f2351c61fc
jacielcastro <jacielcastro100@gmail.com>
2026-08-02 18:41:08 -0300

    :white_check_mark: ATUALIZAÇÃO DO PROJETO

--- src/public/js/funcao/funcao_volta.js
+++ src/public/js/funcao/funcao_volta.js
@@ -1,5 +1,5 @@
 
-document.getElementById("volta_btn").addEventListener("click",() => {
+document.getElementById("voltar_btn").addEventListener("click",() => {
 
     window.location.href = "/html/administrador/dashboard.html";
 
