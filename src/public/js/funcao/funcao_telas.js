4f4e4e1e2d9271b3b5cfa76408307941d30081a0
jacielcastro <jacielcastro100@gmail.com>
2026-08-02 17:32:52 -0300

    :white_check_mark: ATUALIZAÇÃO DO PROJETO

---
+++ src/public/js/funcao/funcao_telas.js
@@ -0,0 +1,8 @@
+
+const botao = document.getElementById("btnNovoFiel");
+
+botao.addEventListener("click", () => {
+
+    window.location.href = "/html/administrador/cadastro_usuario.html";
+
+});
--- src/public/src/Funcao/funcao_telas.js
+++
@@ -1,6 +0,0 @@
-
-export function troca_Tela() {
-    alert('Tela de cadastro de administrador');
-    window.location.href = 'cadastro_usuario.html';
-}
-window.troca_Tela = troca_Tela;
